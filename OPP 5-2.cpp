5d0c730d9336a9dea9025cc76916154d4a6613f7
DONGZIZHI <3401303461@qq.com>
2025-12-24 23:43:31 +0800

    Add files via upload

---
+++ OPP 5-2.cpp
@@ -0,0 +1,130 @@
+﻿#include <iostream>
+using namespace std;
+
+class base {
+public:
+    int a;
+protected:
+    int b;
+private:
+    int c;
+    static int count;
+
+public:
+    base(int a_ = 0, int b_ = 0, int c_ = 0) : a(a_), b(b_), c(c_) { ++count; cout << "[base ctor]\n"; }
+    base(const base& other) : a(other.a), b(other.b), c(other.c) { ++count; cout << "[base copy ctor]\n"; }
+    virtual ~base() { cout << "[base dtor]\n"; }
+
+    void input() {
+        cin >> a >> b >> c;
+    }
+
+    void print() const {
+        cout << "base: a=" << a << " b=" << b << " c=" << c << " count=" << count << endl;
+    }
+
+    static int Manage() {
+        return count;
+    }
+};
+int base::count = 0;
+
+class derived1 : public base {
+public:
+    derived1() : base() { cout << "[derived1 ctor]\n"; }
+    derived1(int a_, int b_, int c_) : base(a_, b_, c_) { cout << "[derived1 ctor]\n"; }
+    ~derived1() override { cout << "[derived1 dtor]\n"; }
+
+    void input() {
+        base::input();
+    }
+
+    void showInside() {
+        cout << "derived1 inside can access a and b; cannot access c directly\n";
+        cout << "inside: a=" << a << " b=" << b << endl;
+        print();
+    }
+};
+
+class derived2 : protected base {
+public:
+    derived2() : base() { cout << "[derived2 ctor]\n"; }
+    derived2(int a_, int b_, int c_) : base(a_, b_, c_) { cout << "[derived2 ctor]\n"; }
+    ~derived2() override { cout << "[derived2 dtor]\n"; }
+
+    void input() {
+        base::input();
+    }
+
+    void showInside() {
+        cout << "derived2 inside can access a and b; cannot access c directly\n";
+        cout << "inside: a=" << a << " b=" << b << endl;
+        print();
+    }
+
+    void showOutsideLike() const {
+        cout << "derived2 expose via public wrapper:\n";
+        print();
+    }
+
+    int getA() const { return a; }
+};
+
+class derived3 : private base {
+public:
+    derived3() : base() { cout << "[derived3 ctor]\n"; }
+    derived3(int a_, int b_, int c_) : base(a_, b_, c_) { cout << "[derived3 ctor]\n"; }
+    ~derived3() override { cout << "[derived3 dtor]\n"; }
+
+    void input() {
+        base::input();
+    }
+
+    void showInside() {
+        cout << "derived3 inside can access a and b; cannot access c directly\n";
+        cout << "inside: a=" << a << " b=" << b << endl;
+        print();
+    }
+
+    void showOutsideLike() const {
+        cout << "derived3 expose via public wrapper:\n";
+        print();
+    }
+
+    int getA() const { return a; }
+};
+
+int main() {
+    cout << "Input for derived1 (a b c): ";
+    derived1 d1;
+    d1.input();
+    cout << "Output derived1:\n";
+    d1.print();
+    cout << "Outside access d1.a = " << d1.a << endl;
+    d1.showInside();
+    cout << "Manage = " << base::Manage() << endl;
+
+    cout << "\nInput for derived2 (a b c): ";
+    derived2 d2;
+    d2.input();
+    cout << "Output derived2:\n";
+    d2.showOutsideLike();
+    cout << "Outside cannot access d2.a directly; wrapper getA() = " << d2.getA() << endl;
+    d2.showInside();
+    cout << "Manage = " << base::Manage() << endl;
+
+    cout << "\nInput for derived3 (a b c): ";
+    derived3 d3;
+    d3.input();
+    cout << "Output derived3:\n";
+    d3.showOutsideLike();
+    cout << "Outside cannot access d3.a directly; wrapper getA() = " << d3.getA() << endl;
+    d3.showInside();
+    cout << "Manage = " << base::Manage() << endl;
+
+    cout << "\nConstructor/Destructor order rule:\n";
+    cout << "construct: base -> derived\n";
+    cout << "destruct : derived -> base\n";
+
+    return 0;
+}
---
+++ OPP 5.cpp
@@ -0,0 +1,113 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+class Point2D {
+private:
+    double x, y;
+
+public:
+    Point2D() : x(0), y(0) {}
+    Point2D(double x_, double y_) : x(x_), y(y_) {}
+    Point2D(const Point2D& other) : x(other.x), y(other.y) {}
+    virtual ~Point2D() {}
+
+    Point2D& move(double dx, double dy) {
+        x += dx;
+        y += dy;
+        return *this;
+    }
+
+    void show() const {
+        cout << "Point2D(" << x << ", " << y << ")" << endl;
+    }
+
+    void input() {
+        cin >> x >> y;
+    }
+
+    double getX() const { return x; }
+    double getY() const { return y; }
+
+    friend double distance(const Point2D& a, const Point2D& b);
+};
+
+double distance(const Point2D& a, const Point2D& b) {
+    double dx = a.x - b.x;
+    double dy = a.y - b.y;
+    return sqrt(dx * dx + dy * dy);
+}
+
+class Point3D : public Point2D {
+private:
+    double z;
+
+public:
+    Point3D() : Point2D(), z(0) {}
+    Point3D(double x_, double y_, double z_) : Point2D(x_, y_), z(z_) {}
+    Point3D(const Point3D& other) : Point2D(other), z(other.z) {}
+    ~Point3D() override {}
+
+    Point3D& move(double dx, double dy, double dz) {
+        Point2D::move(dx, dy);
+        z += dz;
+        return *this;
+    }
+
+    void show() const {
+        cout << "Point3D(" << getX() << ", " << getY() << ", " << z << ")" << endl;
+    }
+
+    void input() {
+        double x, y;
+        cin >> x >> y >> z;
+        Point2D::move(x - getX(), y - getY());
+    }
+
+    friend double distance(const Point3D& a, const Point3D& b);
+};
+
+double distance(const Point3D& a, const Point3D& b) {
+    double dx = a.getX() - b.getX();
+    double dy = a.getY() - b.getY();
+    double dz = a.z - b.z;
+    return sqrt(dx * dx + dy * dy + dz * dz);
+}
+
+int main() {
+    Point2D p1, p2;
+    cout << "Input p1 (x y): ";
+    p1.input();
+    cout << "Input p2 (x y): ";
+    p2.input();
+
+    p1.show();
+    p2.show();
+    cout << "distance(p1, p2) = " << distance(p1, p2) << endl;
+
+    double dx, dy;
+    cout << "Move p1 by (dx dy): ";
+    cin >> dx >> dy;
+    p1.move(dx, dy);
+    p1.show();
+    cout << "distance(p1, p2) = " << distance(p1, p2) << endl;
+
+    Point3D q1, q2;
+    cout << "Input q1 (x y z): ";
+    q1.input();
+    cout << "Input q2 (x y z): ";
+    q2.input();
+
+    q1.show();
+    q2.show();
+    cout << "distance(q1, q2) = " << distance(q1, q2) << endl;
+
+    double dx3, dy3, dz3;
+    cout << "Move q2 by (dx dy dz): ";
+    cin >> dx3 >> dy3 >> dz3;
+    q2.move(dx3, dy3, dz3);
+    q2.show();
+    cout << "distance(q1, q2) = " << distance(q1, q2) << endl;
+
+    return 0;
+}
